b2dfbc6ddb30cd3bb27f11b38fcc9374d17c9bae
Nyeok <dn.shin98@gmail.com>
2024-12-02 18:16:58 +0900

    [Set] dependencies for Emotion and Lottie

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,8 @@ declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module "*.json" {
+  const value: any; // Replace `any` with a specific type if needed
+  export default value;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   root: "./", // 프로젝트 루트 디렉토리
   build: {
     outDir: "dist", // 빌드 결과물 디렉토리
+  },
+  resolve: {
+    alias: {
+      // @/ 로 시작하는 경로를 src/ 로 매핑
+      "@": path.resolve(__dirname, "./src"),
+      "~images": path.resolve(__dirname, "./src/assets/images"),
+    },
   },
 });
