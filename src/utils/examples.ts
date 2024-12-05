3e6165fac8362d190fa7b61bb18938e6cd6dd092
Nyeok <dn.shin98@gmail.com>
2024-12-05 14:45:07 +0900

    [Detach] mocking data

---
+++ src/utils/examples.ts
@@ -0,0 +1,13 @@
+import netflix from "~images/netflix.json";
+import amazonPrime from "~images/amazonprime.json";
+import disney from "~images/disney.json";
+import androidTV from "~images/androidtv.json";
+import fox from "~images/fox.json";
+
+export const contentList = [
+  { title: "Netflix", imgJSON: netflix },
+  { title: "Amazon Prime", imgJSON: amazonPrime },
+  { title: "Disney", imgJSON: disney },
+  { title: "Android TV", imgJSON: androidTV },
+  { title: "Fox", imgJSON: fox },
+];
