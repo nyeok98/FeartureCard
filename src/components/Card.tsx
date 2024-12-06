12a35ffc09dacc056cdefcfec33cefe26958fab2
Nyeok <dn.shin98@gmail.com>
2024-12-07 00:26:45 +0900

    [Add] observer for Card components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,59 @@
 import { Content } from "@/types/content";
 import { cardStyles } from "@/styles/Card.styles";
+import { useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import Lottie from "lottie-react";
 
-type CardProps = {
+type Props = {
   content: Content;
+  parentRef: React.RefObject<HTMLDivElement>;
 };
 
-export const Card = ({ content }: CardProps) => {
+const Card: React.FC<Props> = ({ parentRef, content }) => {
+  const { title, imgJSON } = content;
+  const lottieRef = useRef<any>(null);
+  const [animationProgress, setAnimationProgress] = useState(0);
+
+  const { ref, entry } = useInView({
+    threshold: Array.from({ length: 101 }, (_, i) => i / 100),
+    root: parentRef.current,
+    rootMargin: "0px",
+    triggerOnce: false,
+  });
+
+  useEffect(() => {
+    if (entry) {
+      const intersectionRatio = entry.intersectionRatio;
+      const progress = 1 - intersectionRatio;
+      setAnimationProgress(Math.min(Math.max(progress, 0), 1)); // 0과 1 사이로 제한
+    }
+  }, [entry]);
+
+  useEffect(() => {
+    if (lottieRef.current) {
+      lottieRef.current.goToAndStop(animationProgress * 100, true);
+    }
+  }, [animationProgress]);
+
   return (
-    <div css={cardStyles.self}>
-      <h3>{content.title}</h3>
+    <div ref={ref} css={cardStyles.self}>
+      <Lottie
+        lottieRef={lottieRef}
+        animationData={imgJSON}
+        loop={false}
+        autoplay={false}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+        }}
+      />
+      <h2 css={cardStyles.title}>{title}</h2>
     </div>
   );
 };
+
+export default Card;
--- src/components/CardSlider.tsx
+++ src/components/CardSlider.tsx
@@ -1,14 +1,16 @@
-import { Card } from "./Card";
+import Card from "./Card";
 import { contentList } from "@/utils/examples";
 import { root, title, lastCard } from "@/styles/CardSlider.styles";
+import { useRef } from "react";
 
 export const CardSlider = () => {
+  const ref = useRef(null);
   return (
     <>
       <h1 css={title}>스크롤링 라이브러리</h1>
-      <div css={root}>
+      <div ref={ref} css={root}>
         {contentList.map((content, index) => (
-          <Card key={`card-${index}`} content={content} />
+          <Card key={`card-${index}`} parentRef={ref} content={content} />
         ))}
         <div css={lastCard}>Next</div>
       </div>
