2a80913d16321f49806280247d24caf732510bef
Nyeok <dn.shin98@gmail.com>
2024-12-07 00:17:13 +0900

    [Replace] new assets

--- src/utils/examples.ts
+++ src/utils/examples.ts
@@ -2,12 +2,12 @@ import netflix from "~images/netflix.json";
 import amazonPrime from "~images/amazonprime.json";
 import disney from "~images/disney.json";
 import androidTV from "~images/androidtv.json";
-import fox from "~images/fox.json";
+import sony from "~images/sony.json";
 
 export const contentList = [
   { title: "Netflix", imgJSON: netflix },
   { title: "Amazon Prime", imgJSON: amazonPrime },
   { title: "Disney", imgJSON: disney },
   { title: "Android TV", imgJSON: androidTV },
-  { title: "Fox", imgJSON: fox },
+  { title: "Sony", imgJSON: sony },
 ];
